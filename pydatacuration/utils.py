dc5d2498261760bb48ed265458edece9f4fa11f2
kenlhlui <kenlh.lui@utoronto.ca>
2024-10-17 14:38:46 -0400

    New push

--- pydatacuration/utils.py
+++ pydatacuration/utils.py
@@ -14,3 +14,11 @@ def mk_log_dir():
     log_files_dir_path = r'./log_files/temp_data'
     os.makedirs(log_files_dir_path, exist_ok=True)
 
+def mk_ds_dir():
+    dir_path_list = [r'./dataset', r'./dataset/files', r'./dataset/metadata']
+    for dir_path in dir_path_list:
+        os.makedirs(dir_path, exist_ok=True)
+
+def mk_temp_dir():
+    temp_data_dir = r'./temp_data'
+    os.makedirs(temp_data_dir, exist_ok=True)
