5b5716f84e66e246ff560e788931f887d1e56b63
kenlhlui <kenlh.lui@utoronto.ca>
2024-10-21 10:49:49 -0400

    1. Changed creating directories to DirectoryManager class

--- pydatacuration/utils.py
+++ pydatacuration/utils.py
@@ -1,4 +1,20 @@
 import os
+class DirectoryManager:
+    def __init__(self):
+        self.log_files_dir_path = r'./log_files/temp_data'
+        self.dir_path_list = [r'./dataset', r'./dataset/files', r'./dataset/metadata']
+        self.temp_data_dir = r'./temp_data'
+
+    def mk_log_dir(self):
+        os.makedirs(self.log_files_dir_path, exist_ok=True)
+
+    def mk_ds_dir(self):
+        for dir_path in self.dir_path_list:
+            os.makedirs(dir_path, exist_ok=True)
+
+    def mk_temp_dir(self):
+        os.makedirs(self.temp_data_dir, exist_ok=True)
+
 # Export the structure ('tree') of a directory as plain text
 def list_files(startpath):
     for root, dirs, files in os.walk(startpath):
@@ -8,17 +24,3 @@ def list_files(startpath):
         subindent = ' ' * 4 * (level + 1)
         for f in files:
             print('{}{}'.format(subindent, f))
-
-
-def mk_log_dir():
-    log_files_dir_path = r'./log_files/temp_data'
-    os.makedirs(log_files_dir_path, exist_ok=True)
-
-def mk_ds_dir():
-    dir_path_list = [r'./dataset', r'./dataset/files', r'./dataset/metadata']
-    for dir_path in dir_path_list:
-        os.makedirs(dir_path, exist_ok=True)
-
-def mk_temp_dir():
-    temp_data_dir = r'./temp_data'
-    os.makedirs(temp_data_dir, exist_ok=True)
