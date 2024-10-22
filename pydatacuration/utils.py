f10367cf2129f92c7f44fe055238d3d44e5901c2
Ken Lui <kenlh.lui@utoronto.ca>
2024-10-22 17:04:34 -0400

    1. Added check_special_char function 2. Added check_special_char function 3. Added check_file_ext function 4. Added doc strings to some functions

--- pydatacuration/utils.py
+++ pydatacuration/utils.py
@@ -1,22 +1,34 @@
 import os
+from pathlib import Path
 class DirectoryManager:
+    """This class is used to manage the directories in the project
+    """
+
     def __init__(self):
         self.log_files_dir_path = r'./log_files/temp_data'
         self.dir_path_list = [r'./dataset', r'./dataset/files', r'./dataset/metadata']
         self.temp_data_dir = r'./temp_data'
 
     def mk_log_dir(self):
+        """Create the log directory
+        """
         os.makedirs(self.log_files_dir_path, exist_ok=True)
 
     def mk_ds_dir(self):
+        """Create the dataset directory
+        """
         for dir_path in self.dir_path_list:
             os.makedirs(dir_path, exist_ok=True)
 
     def mk_temp_dir(self):
+        """Create the temp directory
+        """
         os.makedirs(self.temp_data_dir, exist_ok=True)
 
 # Export the structure ('tree') of a directory as plain text
 def list_files(startpath):
+    """List the files in the directory
+    """
     for root, dirs, files in os.walk(startpath):
         level = root.replace(startpath, '').count(os.sep)
         indent = ' ' * 4 * (level)
@@ -24,3 +36,14 @@ def list_files(startpath):
         subindent = ' ' * 4 * (level + 1)
         for f in files:
             print('{}{}'.format(subindent, f))
+
+
+def get_file_name(file):
+    """Get the file name from the file path
+    """
+    return Path(file).stem
+
+def get_file_extension(file):
+    """Get the file extension from the file path
+    """
+    return Path(file).suffix
