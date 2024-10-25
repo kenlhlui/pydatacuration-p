daae455270ce9f0a1dcf3c0c085738e9fe351476
kenlhlui <kenlh.lui@utoronto.ca>
2024-10-24 23:14:11 -0400

    1. Added downloads.py as a module for downloading dataset metadata and zip file.

---
+++ pydatacuration/downloads.py
@@ -0,0 +1,58 @@
+import sys
+import httpx
+import orjson
+
+class Downloads:
+    def __init__(self, base_url, api_token, pid):
+        self.base_url = base_url
+        self.api_token = api_token
+        self.pid = pid
+        self.client = httpx.Client(headers={'X-Dataverse-key': self.api_token}, timeout=None)
+
+    def get_ds_metadata(self):
+        """Get metadata of a dataset
+        
+        Returns:
+            dict: Metadata of the dataset
+        """
+        url = f"{self.base_url}/api/datasets/:persistentId/?persistentId={self.pid}"
+        try:
+            response = self.client.get(url)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+
+            with open('dataset/metadata/ds_metadata.json', 'w', encoding='utf-8') as f:
+                f.write(orjson.dumps(response.json(), option=orjson.OPT_INDENT_2).decode())
+
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP error occurred: {e}")
+            sys.exit(1)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            sys.exit(1)
+
+    def get_ds_zip(self):
+        # TODO: Change to 'Download By Dataset By Version' API, if possible (it's not working now)
+        """Get a dataset as a zip file
+
+        Returns:
+            str: Path to the downloaded zip file
+        """
+        file_path = 'temp_data/ds.zip'
+        url = self.base_url + 'api/access/dataset/:persistentId/?persistentId=' + self.pid
+
+        try:
+            with self.client.stream("GET", url) as response:
+                response.raise_for_status()
+                with open(file_path, 'wb') as f:
+                    for chunk in response.iter_bytes():
+                        f.write(chunk)
+            return file_path
+
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP error occurred: {e}")
+            sys.exit(1)
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            sys.exit(1)
