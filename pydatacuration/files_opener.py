5ab5ca87e77f096df38699284eadde81a658f67e
Ken Lui <kenlh.lui@utoronto.ca>
2024-10-30 22:08:36 -0400

    1. Updated open_file function in files_opener module

--- pydatacuration/files_opener.py
+++ pydatacuration/files_opener.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 from PIL import Image
 import chardet
+import netCDF4 as nc
 
 class FilesOpener:
     def __init__(self, file):
@@ -17,33 +18,44 @@ class FilesOpener:
         with open(self.file, 'rb') as f:
             return chardet.detect(f.read()).get('encoding', None)
 
-    def open_image_file(self):
+    def _open_image_file(self):
         """Open an image file
         
         Returns:
             Image: The image object.
         """
-        image_file_list = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.tif'] # TODO: 1. Add more image file extensions; 2. Use a function to allow import a list from a configuration file;
-        file_ext = Path(self.file).suffix
-        if file_ext and file_ext.lower() in image_file_list:
-            try:
-                Image.open(self.file)
-                return True, self.file
-            except (ValueError, Image.UnidentifiedImageError, OSError):
-                return False, self.file
-    def open_text_file(self):
-        """Open a text file and return the encoding of the file
+        try:
+            Image.open(self.file)
+            return True, self.file
+        except (ValueError, Image.UnidentifiedImageError, OSError):
+            return False, self.file
+    
+    def _open_netcdf_file(self):
+        """Open a NetCDF file
         
         Returns:
-            str: The text content.
+            str: The NetCDF file object.
         """
-        text_file_list = ['.txt', '.csv', '.tsv']
-        file_ext = Path(self.file).suffix
-        if file_ext and file_ext.lower() in text_file_list:
-            encoding = self._get_file_encoding()  # Check the file encoding
-            try:
-                with open(self.file, 'r', encoding=encoding):
-                    return True, self.file, encoding
-            except (Exception, UnicodeDecodeError):
-                return False, self.file, None
-        return False, self.file, None
+        try:
+            nc.Dataset(self.file, 'r')
+            return True, self.file
+        except (Exception, OSError):
+            return False, self.file
+
+    def open_file(self):
+        """Open a file
+        
+        Returns:
+            str: The file object
+        """
+        if os.path.isfile(self.file):
+            file_ext = Path(self.file).suffix
+            image_file_list = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.tif'] # TODO: 1. Add more image file extensions; 2. Use a function to allow import a list from a configuration file;
+            # text_file_list = ['.txt', '.csv', '.tsv'] TODO: Placeholder for text file extensions
+            netcdf_file_list = ['.nc']
+            if file_ext.lower() in image_file_list:
+                return self._open_image_file()
+
+            if file_ext.lower() in netcdf_file_list:
+                return self._open_netcdf_file()
+        return None, self.file
