bc89929f1ddc5169d1450320aa3f1cca01ad29f4
Ken Lui <kenlh.lui@utoronto.ca>
2024-10-30 20:06:46 -0400

    Squashed commit of the following:
    commit 7dd49101faf9234e566e6e5103b8ee6cdb77d409
    Author: Ken Lui <kenlh.lui@utoronto.ca>
    Date:   Wed Oct 30 20:06:14 2024 -0400
    
        1. Added chardet and netcdf4 libraries to pyproject.toml
    
    commit 06e177fd1938d5886c8ff1662a87b203297a269f
    Author: Ken Lui <kenlh.lui@utoronto.ca>
    Date:   Wed Oct 30 20:05:06 2024 -0400
    
        1. Added open_text_file function to files_openner to check the encoding of text file

--- pydatacuration/files_opener.py
+++ pydatacuration/files_opener.py
@@ -1,10 +1,21 @@
+# pylint: disable=C0301
 import os
 from pathlib import Path
 from PIL import Image
+import chardet
 
 class FilesOpener:
     def __init__(self, file):
         self.file = file
+
+    def _get_file_encoding(self):
+        """Check the file encoding
+        
+        Returns:
+            str: The file encoding.
+        """
+        with open(self.file, 'rb') as f:
+            return chardet.detect(f.read()).get('encoding', None)
 
     def open_image_file(self):
         """Open an image file
@@ -12,11 +23,27 @@ class FilesOpener:
         Returns:
             Image: The image object.
         """
-        image_file_list = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.tif']
+        image_file_list = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.tif'] # TODO: 1. Add more image file extensions; 2. Use a function to allow import a list from a configuration file;
         file_ext = Path(self.file).suffix
         if file_ext and file_ext.lower() in image_file_list:
             try:
                 Image.open(self.file)
-                return True
+                return True, self.file
             except (ValueError, Image.UnidentifiedImageError, OSError):
-                return False
+                return False, self.file
+    def open_text_file(self):
+        """Open a text file and return the encoding of the file
+        
+        Returns:
+            str: The text content.
+        """
+        text_file_list = ['.txt', '.csv', '.tsv']
+        file_ext = Path(self.file).suffix
+        if file_ext and file_ext.lower() in text_file_list:
+            encoding = self._get_file_encoding()  # Check the file encoding
+            try:
+                with open(self.file, 'r', encoding=encoding):
+                    return True, self.file, encoding
+            except (Exception, UnicodeDecodeError):
+                return False, self.file, None
+        return False, self.file, None
