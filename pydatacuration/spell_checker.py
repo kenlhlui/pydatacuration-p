7fc5e93ae296e815e202b744723ea2687979efec
Ken Lui <kenlh.lui@utoronto.ca>
2024-11-01 16:28:57 -0400

    1. Added _get_stop_words function to download the stopwords and place them in res/nltk_data 2. Fixed some naming conventions

--- pydatacuration/spell_checker.py
+++ pydatacuration/spell_checker.py
@@ -1,3 +1,4 @@
+import os
 import string
 from spellchecker import SpellChecker
 import nltk
@@ -5,8 +6,27 @@ import nltk
 class SpellCheckerCustomized():
     def __init__(self):
         self.spell = SpellChecker()
+    #TODO: Add a function/amend clean_text function to remove the html tags, if appeared.
+    #TODO: Error handling if the input is not a string/list.
 
-    def clean_text(self, text):
+    def _get_stop_words(self):
+        """
+        Get the stop words from the nltk library.
+
+        Returns:
+            list: The stop words.
+        """
+        current_path = os.getcwd()
+        nltk_stopwords_path = os.path.join(current_path, 'res', 'nltk_data')
+        if not os.path.exists(nltk_stopwords_path):
+            os.mkdir(nltk_stopwords_path)
+            nltk.download('stopwords', download_dir=nltk_stopwords_path, quiet=True)
+            nltk.data.path.append(nltk_stopwords_path)
+
+        else:
+            nltk.data.path.append(nltk_stopwords_path)
+
+    def _clean_text(self, text):
         """
         Clean the text by removing special characters, numbers, and stop words.
 
@@ -20,7 +40,7 @@ class SpellCheckerCustomized():
         # Trun text in to list
         translator = str.maketrans('', '', string.punctuation)
         text = text.translate(translator)
-        
+
         text = text.split()
 
         # Remove the words in Capital letters
@@ -35,6 +55,9 @@ class SpellCheckerCustomized():
         # Remove numbers
         text = [word for word in text if not word.isdigit()]
 
+        # Get and load the stop words
+        self._get_stop_words()
+
         # Load stop words
         stop_words = nltk.corpus.stopwords.words('english')
 
@@ -43,7 +66,7 @@ class SpellCheckerCustomized():
 
         return text_filtered
 
-    def check_spelling(self, list_of_words):
+    def check_spelling(self, list_of_words: list):
         """
         Check the spelling of the text.
         
@@ -55,10 +78,9 @@ class SpellCheckerCustomized():
         """
         misspelled_words = self.spell.unknown(list_of_words)
 
-        print(misspelled_words)
         return misspelled_words, bool(misspelled_words)
 
-    def main(self, list_of_words: list):
+    def spell_checker(self, list_of_words: list):
         """
         Check the spelling of the text.
         
@@ -68,6 +90,6 @@ class SpellCheckerCustomized():
         Returns:
             bool: True if there are misspelled words, False otherwise.
         """
-        list_of_words = self.clean_text(list_of_words)
+        list_of_words = self._clean_text(list_of_words)
         return self.check_spelling(list_of_words)
 
