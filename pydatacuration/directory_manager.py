7a4d696781b61af0956b5900ed163ab5038de7b8
Ken Lui <kenlh.lui@utoronto.ca>
2025-01-14 17:03:05 -0500

    1. Applied ruff style 2. Changed using Pathlib

--- pydatacuration/directory_manager.py
+++ pydatacuration/directory_manager.py
@@ -1,13 +1,14 @@
-# pylint: disable=C0301
+"""This module is used to manage the directories in the project."""
 
 import os
+from pathlib import Path
+
 
 class DirectoryManager:
-    """This class is used to manage the directories in the project
-    """
+    """This class is used to manage the directories in the project."""
 
-    def __init__(self, workdir: str = None):
-        """Initialize the class
+    def __init__(self, workdir: str = None) -> None:
+        """Initialize the class.
 
         Args:
             workdir (str): The working directory.
@@ -19,45 +20,58 @@ class DirectoryManager:
 
         Args:
             dir_path (str): The path to the directory.
-        
+
         This method updates self.workdir to a valid path (user's choice if valid, otherwise current directory).
         """
-        self.workdir = dir_path if dir_path and os.path.exists(dir_path) else os.path.join(os.getcwd(), 'workdir')
-        if not os.path.exists(dir_path):
-            print(f"The user defined directory does not exist. The current directory will be used: {self.workdir}")
+        dir_path = Path(dir_path)  # Ensure dir_path is a Path object
+        self.workdir = dir_path if dir_path.exists() else Path(Path.cwd(), 'workdir')
+        if not dir_path.exists():
+            print(f'The user defined directory does not exist. The current directory will be used: {self.workdir}')
 
-    def _mk_log_dir(self):
-        """Create the log directory
+    def _mk_log_dir(self) -> str:
+        """Create the log directory.
+
+        Returns:
+            str: The path to the log directory.
         """
-        log_files_dir = os.path.join(self.workdir, 'log_files', 'temp_data')
-        os.makedirs(log_files_dir, exist_ok=True)
+        log_files_dir = Path(self.workdir, 'log_files', 'temp_data')
+        log_files_dir.mkdir(parents=True, exist_ok=True)
 
-        return log_files_dir
+        return log_files_dir.resolve()
 
-    def _mk_ds_dir(self):
-        """Create the dataset directory
+    def _mk_ds_dir(self) -> str:
+        """Create the dataset directory.
+
+        Returns:
+            str: The path to the dataset directory.
         """
         dir_path_list = [
-            os.path.join(self.workdir, 'dataset'),
-            os.path.join(self.workdir, 'dataset', 'files'),
-            os.path.join(self.workdir, 'dataset', 'metadata')
+            Path(self.workdir, 'dataset').resolve(),
+            Path(self.workdir, 'dataset', 'files').resolve(),
+            Path(self.workdir, 'dataset', 'metadata').resolve(),
         ]
 
         for dir_path in dir_path_list:
-            os.makedirs(dir_path, exist_ok=True)
+            Path.mkdir(dir_path, parents=True, exist_ok=True)
 
         return dir_path_list[0]
 
-    def _mk_temp_dir(self):
-        """Create the temp directory
+    def _mk_temp_dir(self) -> str:
+        """Create the temp directory.
+
+        Returns:
+            str: The path to the temp directory.
         """
-        temp_data_dir = os.path.join(self.workdir, 'temp_data')
-        os.makedirs(temp_data_dir, exist_ok=True)
+        temp_data_dir = Path(self.workdir, 'temp_data')
+        Path.mkdir(temp_data_dir, parents=True, exist_ok=True)
 
-        return temp_data_dir
+        return temp_data_dir.resolve()
 
-    def make_dirs(self):
-        """Create the directories
+    def make_dirs(self) -> tuple:
+        """Create the directories.
+
+        Returns:
+            tuple: A tuple containing the workdir, log_files_dir, ds_dir, temp_data_dir directories.
         """
         log_files_dir = self._mk_log_dir()
         ds_dir = self._mk_ds_dir()
