483f313e66a88854c5885b132be20a91d64b46fd
Ken Lui <116421546+kenlhlui@users.noreply.github.com>
2025-02-25 16:00:59 -0500

    Formatting

--- pydatacuration/template_generation.py
+++ pydatacuration/template_generation.py
@@ -1,16 +1,27 @@
-# pylint: disable=C0114, C0301, C0116
+"""Generates a report based on a template and data from a JSON file."""
 import orjson
 import pandas as pd
 from jinja2 import Template
 
-def read_template_json():
-    with open('./res/template.json', 'r', encoding='utf-8') as file:
-        template = orjson.loads(file.read()) # pylint: disable=E1101
-    return template
 
-def generate_report(template_dict):
+def read_template_json() -> dict:
+    """Reads the template.json file and returns it as a dictionary.
+
+    Returns:
+        dict: The template as a dictionary.
+    """
+    with open('./res/template.json', encoding='utf-8') as file:
+        return orjson.loads(file.read())
+
+
+def generate_report(template_dict: dict) -> None:
+    """Generates a report based on the provided template dictionary.
+
+    Args:
+        template_dict (dict): The template dictionary.
+    """
     # Read the template.csv file
-    with open('./res/template.csv', 'r', encoding='ISO-8859-1') as f:
+    with open('./res/template.csv', encoding='ISO-8859-1') as f:
         template_string = f.read()
     report = Template(template_string)
 
@@ -20,5 +31,6 @@ def generate_report(template_dict):
         f.write(rendered)
 
     # Convert the rendered csv file to an Excel file
-    pd.read_csv('./workdir/log_files/temp_data/render_log.csv', keep_default_na=False).to_excel('./workdir/log_files/render_log.xlsx', index=False, na_rep='NA')
+    pd.read_csv('./workdir/log_files/temp_data/render_log.csv', keep_default_na=False).to_excel(
+        './workdir/log_files/render_log.xlsx', index=False, na_rep='NA')
     print('\nReport generated. See the workdir/log_files/render_log.xlsx file for the report.')
