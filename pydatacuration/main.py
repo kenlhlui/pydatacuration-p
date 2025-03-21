b0903b462fa981d01e7f79398bcb5751cac371ba
Ken Lui <116421546+kenlhlui@users.noreply.github.com>
2025-03-20 21:55:08 -0400

    1. Replace log_dir from workdir in log_generation module for better path management

--- pydatacuration/main.py
+++ pydatacuration/main.py
@@ -94,7 +94,7 @@ def cli(
     template_dict = checker.run_checks()
 
     # Generate the report
-    generate_log = log_generation.GenerateLog(workdir_path, base_url, ds_metadata, ticket_number)
+    generate_log = log_generation.GenerateLog(log_files_dir, base_url, ds_metadata, ticket_number)
     generate_log.generate_report_xlsx(template_dict)
     generate_log.generate_report_doc(template_dict, 'medium')  # medium-level report
     generate_log.generate_report_doc(template_dict, 'high')  # high-level report
