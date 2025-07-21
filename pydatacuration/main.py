040d5e18d87957f0d7c88c0c07986735dc81cbe8
Ken Lui <116421546+kenlhlui@users.noreply.github.com>
2025-07-21 11:28:29 -0400

    Moved all the template generation fields to template_dict for centralized management. Updated the template docx files & template_dict.json accordingly

--- pydatacuration/main.py
+++ pydatacuration/main.py
@@ -118,12 +118,11 @@ def gen_curation_report(
         template_dict = checker.run_checks()
 
         # Generate the report
-        generate_log = log_generation.GenerateLog(log_files_dir, base_url, ds_metadata, ticket_number)
+        generate_log = log_generation.GenerateLog(log_files_dir, base_url, ds_metadata, ticket_number, template_dict)
         # ! Disable generation of xlsx report for now
         # generate_log.generate_report_xlsx(template_dict)
         generate_log.generate_report_doc(template_dict, 'medium')  # medium-level report
         generate_log.generate_report_doc(template_dict, 'high')  # high-level report
-        generate_log.generate_project_metadata()
 
         # Export the template dict to JSON for debugging purposes
         orjson_export(log_files_dir.joinpath('template_dict.json'), template_dict)
