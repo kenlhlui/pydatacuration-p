3c02da966dedc7d91fd421c5eb08b91b5520af5f
Ken Lui <116421546+kenlhlui@users.noreply.github.com>
2025-07-24 16:40:43 -0400

    Move resetForm function to separate JavaScript file for better organization

---
+++ pydatacuration/frontend/js/restformbutton.js
@@ -0,0 +1,5 @@
+function resetForm() {
+            document.getElementById('setup-form').reset();
+            document.getElementById('error-message').style.display = 'none';
+            document.getElementById('success-message').style.display = 'none';
+        }
