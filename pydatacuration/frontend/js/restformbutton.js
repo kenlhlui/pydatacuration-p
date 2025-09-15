37747e98c7f186588f2fce9e97c3eb6649cff97e
Ken Lui <116421546+kenlhlui@users.noreply.github.com>
2025-09-15 12:59:13 -0400

    feat: consolidated the js files

--- pydatacuration/frontend/js/restformbutton.js
+++
@@ -1,5 +0,0 @@
-function resetForm() {
-            document.getElementById('setup-form').reset();
-            document.getElementById('error-message').style.display = 'none';
-            document.getElementById('success-message').style.display = 'none';
-        }
