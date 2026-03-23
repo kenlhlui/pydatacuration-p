8086b1821167f869b5fd632b50a3691b214d215e
Ken Lui <116421546+kenlhlui@users.noreply.github.com>
2026-03-23 12:15:59 -0400

    refactor: removed the unused code

--- pydatacuration/backend/api.py
+++ pydatacuration/backend/api.py
@@ -149,8 +149,3 @@ def check(
 def report() -> None:
     pass
     # TODO: this api endpoint might not be necessary. Might just keep it in the CLI for now
-
-
-@router.post('/run-all')
-def run_all() -> None:
-    pass
