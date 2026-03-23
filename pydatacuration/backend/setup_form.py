89e5726ceb6a85aa0b1b249dc96b43694077bb1e
Ken Lui <116421546+kenlhlui@users.noreply.github.com>
2026-03-23 16:44:05 -0400

    fix: Field/serializer ordering

--- pydatacuration/backend/setup_form.py
+++ pydatacuration/backend/setup_form.py
@@ -12,34 +12,6 @@ class SetupForm(BaseModel):
     main_dir: str = 'workdir'
     res_dir: str = 'res'
 
-    @field_serializer('base_url')
-    def serialize_base_url(self, v: HttpUrl | None) -> str | None:
-        """Serialize HttpUrl to string.
-
-        Args:
-            v (HttpUrl | None): URL value.
-
-
-        Returns:
-            str | None: String URL.
-
-        """
-        return str(v) if v else None
-
-    @field_serializer('main_dir')
-    def serialize_main_dir(self, v: str) -> str:
-        """Resolve and serialize path to string.
-
-        Args:
-            v (str): Path string.
-
-
-        Returns:
-            str: Resolved path string.
-
-        """
-        return str(Path(v).resolve())
-
     base_url: HttpUrl | None = None
     api_token: str | None = None
 
@@ -53,3 +25,31 @@ class SetupForm(BaseModel):
     force_delete: bool = False
     check_zip: bool = True
     checklist: str = ''
+
+    @field_serializer('base_url')
+    def serialize_base_url(self, v: HttpUrl | None) -> str | None:  # noqa: PLR6301
+        """Serialize HttpUrl to string.
+
+        Args:
+            v (HttpUrl | None): URL value.
+
+
+        Returns:
+            str | None: String URL.
+
+        """
+        return str(v) if v else None
+
+    @field_serializer('main_dir')
+    def serialize_main_dir(self, v: str) -> str:  # noqa: PLR6301
+        """Resolve and serialize path to string.
+
+        Args:
+            v (str): Path string.
+
+
+        Returns:
+            str: Resolved path string.
+
+        """
+        return str(Path(v).resolve())
