3852c4e930cde58a4caba2b90b717614d971a8c8
Ken Lui <116421546+kenlhlui@users.noreply.github.com>
2026-03-24 18:18:38 -0400

    WIP: workable script for the stress test using api

--- stress-test.py
+++ stress-test.py
@@ -1,5 +1,7 @@
 """Stress test script for the API endpoint and the application."""
 
+import time
+
 import httpx
 from loguru import logger
 
@@ -9,26 +11,41 @@ from pydatacuration.backend.models.setup_form import SetupDefaults
 from pydatacuration.backend.models.setup_form import SetupForm
 
 
-def run_curation(setup_form_instance: SetupForm, app_settings: AppSettings):
-    logger.debug(app_settings.app_port)
-    httpx.post(
-        f'http://localhost:{app_settings.app_port}/api/run-curation',
-        json=setup_form_instance.model_dump(mode='json'),
-        timeout=None,
-    )
+def run_curation(setup_form_instance: SetupForm | SetupDefaults, app_settings: AppSettings) -> None:
+    start_time = time.time()
+    logger.info(f'Running crawl for {setup_form_instance.pid}')
+    with httpx.Client() as client:
+        client.post(
+            f'http://localhost:{app_settings.app_port}/api/run-curation',
+            json=setup_form_instance.model_dump(mode='json'),
+            timeout=None,
+        )
+    end_time = time.time()
+    logger.info(f'Crawl completed for {setup_form_instance.pid} in {end_time - start_time:.2f} seconds')
 
 
-def dois() -> list[str]:
+def datasets() -> dict[str, str]:
     """Return a list of DOIs for testing."""
-    return [
-        '10.5072/TOTALLYFICTICIOUS',
-        '10.5072/EVENMOREFICTICIOUS',
-        '10.5072/ANOTHERFAKEONE',
-    ]
+    return {
+        'CUR-001': 'doi:10.80240/FK2/HT4WPP',
+        # 'CUR-002': 'doi:10.80240/FK2/HCUJKX',
+        # 'CUR-003': 'doi:10.80240/FK2/CFUXY6',
+        # 'CUR-004': 'doi:10.80240/FK2/WTMREZ',
+        'CUR-005': 'doi:10.80240/FK2/TSHCUL',
+    }
 
 
 if __name__ == '__main__':
     default_setup = SetupDefaults()
     app_settings = AppSettings()
 
-    run_curation(default_setup, app_settings)
+    datasets_dict = datasets()
+    for ticket_number, pid in datasets_dict.items():
+        setup_data = default_setup.model_dump()
+        setup_data['ticket_number'] = ticket_number
+        setup_data['pid'] = pid
+        setup_form = SetupForm(**setup_data, main_dir=app_settings.main_dir)
+        try:
+            run_curation(setup_form, app_settings)
+        except Exception as e:
+            logger.error(f'Error occurred while running curation for {ticket_number}: {e}')
